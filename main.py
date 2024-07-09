dd4b12b004c6cfce356306a3edf73c36a8a8dbcb
Screen117 <j_goom@yahoo.com>
2024-07-09 12:36:46 -0400

    finshed book bot

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ def main():
     num_words = get_num_words(text)
     low_string = make_lower(text)
     characters = character_count(low_string)
-    print(characters)
+    chars_sorted_list = chars_dict_to_sorted_list(characters)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in document")
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("---End of Report---")
+    
+
 def get_book_text(path):    
     with open(path) as f:
         return f.read()
@@ -28,6 +39,30 @@ def character_count(low_string):
 
         
     return character_dict
+
+def report_gen(characters):
+    for idx in characters:
+        
+        letter= characters.split()
+        print(letter)
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char":ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+    
+
+    
+    
+
+
+
     
         
 main()
